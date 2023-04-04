574f148b303142c221b7261ab9c79b3d7447ddab
jwselu <90157025+jwselu@users.noreply.github.com>
2023-04-04 00:22:36 -0500

    Update ticketsystem.js

--- ticketsystem.js
+++ ticketsystem.js
@@ -33,11 +33,11 @@ app.get('/say/:name', function(req, res) {
 });
 
 // Route to access database
-// This is used to display an item with a provided _id number.
+// This is used to display an item with a provided type.
 // Ex. https://mongorender-t4qw.onrender.com/api/mongo/12345
 app.get('/api/mongo/:item', function(req, res) {
 const client = new MongoClient(uri);
-const searchKey = "{ _id: '" + req.params.item + "' }";
+const searchKey = "{type: '" + req.params.item + "' }";
 console.log("Looking for: " + searchKey);
 
 async function run() {
@@ -51,7 +51,7 @@ async function run() {
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
     // But we will use the parameter provided with the route
-    const query = { _id: req.params.item };
+    const query = { type: req.params.item };
 
     const part = await parts.findOne(query);
     console.log(part);
