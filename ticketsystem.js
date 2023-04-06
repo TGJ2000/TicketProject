ea813be5ecfe72ae9e3c8221b6a0b835ea038783
jwselu <90157025+jwselu@users.noreply.github.com>
2023-04-06 16:55:29 -0400

    Changed "Part" to "Ticket

--- ticketsystem.js
+++ ticketsystem.js
@@ -32,6 +32,34 @@ app.get('/say/:name', function(req, res) {
   res.send('Hello ' + req.params.name + '!');
 });
 
+app.get('/rest/ticket/', function(req, res) {
+const client = new MongoClient(uri);
+const searchKey = "{id: '" + req.params.theId + "' }";
+console.log("Looking for: " + searchKey);
+
+async function run() {
+  try {
+    // From MongoDB Database Page --> Browse Collections --> Collections tab
+    // client.db has the database.collection underneath it (in green font).
+    // Make sure the client.db name is the same as your project's database name
+    const database = client.db('Cluster0');
+    const tickets = database.collection('MyDB');
+
+    //We will use the parameter provided with the route
+    const query = {id: req.params.theId };
+
+    const ticket = await tickets.findOne(query);
+    console.log(ticket);
+    res.send('Found this: ' + JSON.stringify(ticket));  //Use stringify to print a json
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+});
+
 app.get('/rest/ticket/:theId', function(req, res) {
 const client = new MongoClient(uri);
 const searchKey = "{id: '" + req.params.theId + "' }";
@@ -43,16 +71,14 @@ async function run() {
     // client.db has the database.collection underneath it (in green font).
     // Make sure the client.db name is the same as your project's database name
     const database = client.db('Cluster0');
-    const parts = database.collection('MyDB');
+    const tickets = database.collection('MyDB');
 
-    // Hardwired Query for a part that has partID '12345'
-    // const query = { partID: '12345' };
-    // But we will use the parameter provided with the route
-    const query = { partID: req.params.item };
+    //We will use the parameter provided with the route
+    const query = {id: req.params.theId };
 
-    const part = await parts.findOne(query);
-    console.log(part);
-    res.send('Found this: ' + JSON.stringify(part));  //Use stringify to print a json
+    const ticket = await tickets.findOne(query);
+    console.log(ticket);
+    res.send('Found this: ' + JSON.stringify(ticket));  //Use stringify to print a json
 
   } finally {
     // Ensures that the client will close when you finish/error
@@ -61,6 +87,9 @@ async function run() {
 }
 run().catch(console.dir);
 });
+
+
+
 
 // Route to access database
 // This is used to display an item with a provided part number.
